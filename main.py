e514dace378313ccfd0fb074f2bd85e68d71c18c
Jonah Ramponi <jonah.ramponi@smartr.ai>
2024-06-15 00:13:00 +0100

    test

--- main.py
+++ main.py
@@ -2,29 +2,28 @@
 Entry point script
 """
 
-import subprocess
-import sys
-import threading
+# import subprocess
+# import sys
 
 
-def run_fastapi_server(port=8000):
-    cmd = [sys.executable, "weekly_mileage_modelling/api/run.py", "--port", str(port)]
-    subprocess.Popen(cmd)
+# def run_fastapi_server(port=8000):
+#     cmd = [sys.executable, "weekly_mileage_modelling/api/run.py", "--port", str(port)]
+#     subprocess.Popen(cmd)
 
 
-def run_streamlit_app(port=8501):
-    cmd = [
-        sys.executable,
-        "-m",
-        "streamlit",
-        "run",
-        "frontend/interface.py",
-        "--server.port",
-        str(port),
-    ]
-    subprocess.Popen(cmd)
+# def run_streamlit_app(port=8501):
+#     cmd = [
+#         sys.executable,
+#         "-m",
+#         "streamlit",
+#         "run",
+#         "frontend/interface.py",
+#         "--server.port",
+#         str(port),
+#     ]
+#     subprocess.Popen(cmd)
 
 
-if __name__ == "__main__":
-    run_fastapi_server(port=8000)
-    # run_streamlit_app(port=8501)
+# if __name__ == "__main__":
+#     run_fastapi_server(port=8000)
+#     # run_streamlit_app(port=8501)
